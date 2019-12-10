ee594bf56a9651905f95c90e76fe068daeaae5aa
Eric Clack <eric@bn7.net>
2019-12-10 15:53:03 +0000

    Added part3

---
+++ candy3.py
@@ -0,0 +1,37 @@
+import random
+
+WIDTH = 400
+HEIGHT = 560
+TITLE = 'Candy Crush'
+
+cursor = Actor('selected', topleft=(0,0))
+
+board = []
+for row in range(14):
+    # Make a list of 10 random tiles
+    tiles = [random.randint(1,8) for _ in range(10)]
+    board.append(tiles)
+
+def draw():
+     for y in range(14):
+        for x in range(10):
+            tile = board[y][x]
+            screen.blit(str(tile), (x * 40, y * 40))
+     cursor.draw()
+
+def cursor_tile_pos():
+    return (int(cursor.x // 40)-1, int(cursor.y // 40))
+
+def on_key_up(key):
+    x, y = cursor_tile_pos()
+    print(x,y)
+    if key == keys.LEFT and x > 0:
+        cursor.x -= 40
+    if key == keys.RIGHT and x < 8:
+        cursor.x += 40
+    if key == keys.UP and y > 0:
+        cursor.y -= 40
+    if key == keys.DOWN and y < 13:
+        cursor.y += 40
+    if key == keys.SPACE:
+        board[y][x], board[y][x+1] = board[y][x+1], board[y][x]
--- doc/part3.rst
+++ doc/part3.rst
@@ -26,7 +26,7 @@ So we need to check the cursor's current position and only allow it to move if i
 
 It's not obvious but we have some repetition in our code now. We are using the numbers 8 and 13 above, which relate to the board size, as used in the loops to set up the board.
 
-* Try changing the board size and screen size to see this limitation in our code, e.g.:
+* Try changing the board size and screen size to see this limitation in our code, you'll need to change :code:`HEIGHT` and the two loop ranges as follows (the elipses ... represent existing code):
 
 .. code:: python
 
@@ -38,10 +38,22 @@ It's not obvious but we have some repetition in our code now. We are using the n
 
    def draw():
      for y in range(18):
+         ...
 
 Now try moving the cursor to the bottom.
 
 *Can you see the relationship between these values?*
+
+
+D.R.Y.
+------
+
+...
+
+Matching Tiles
+--------------
+
+...
 
 
 
